f9f4870ce8cc9da5aab6dca730e5895a85cbf100
andreinalivaika <nav.testsw@gmail.com>
2023-05-28 14:11:27 +0300

    05 add custom method to get all posts and check their length

--- cypress/e2e/Practice/cypress-commands.cy.js
+++ cypress/e2e/Practice/cypress-commands.cy.js
@@ -1,15 +1,24 @@
 describe("Custom Cypress Commands", () => {
-  it("uses cy.getAllPosts()to retrieve all posts from the /api/posts endpoint", () => {
-    // Create a custom Cypress Command called 'getAllPosts' which uses
-    // cy.request() to get all of the posts from the /api/posts endpoint
-    // Then use this custom command in this test to assert that the length of the posts
-    // returned is equal to 2
-  });
+    // beforeEach(() => {
+    //     cy.intercept("GET", "http://localhost:3000/api/posts", (req) => {
+    //     }).as("posts");
+    //     cy.visit("http://localhost:3000");
+    // });
 
-  it("uses cy.getFirstPost() to retrieve the first post from the /api/posts endpoint", () => {
-    // Create a custom Cypress Command called 'getFirstPost' which uses
-    // cy.request() to get the first post from the /api/posts endpoint
-    // Then use this custom command in this test to assert that the id of the first post
-    // is equal to 'pre-rendering'
-  });
+    it("uses cy.getAllPosts()to retrieve all posts from the /api/posts endpoint", () => {
+      // Create a custom Cypress Command called 'getAllPosts' which uses
+      // cy.request() to get all of the posts from the /api/posts endpoint
+      // Then use this custom command in this test to assert that the length of the posts
+      // returned is equal to 2
+      cy.getAllPosts().then((posts) => {
+          cy.wrap(posts).its("length").should("equal", 2);
+      })
+    });
+
+    it("uses cy.getFirstPost() to retrieve the first post from the /api/posts endpoint", () => {
+      // Create a custom Cypress Command called 'getFirstPost' which uses
+      // cy.request() to get the first post from the /api/posts endpoint
+      // Then use this custom command in this test to assert that the id of the first post
+      // is equal to 'pre-rendering'
+    });
 });
